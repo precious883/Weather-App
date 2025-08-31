fa8ac4c085937dff8a0e536cc81c8a0fb0c83808
precious883 <preciouspeter25@yahoo.com>
2025-08-31 05:38:41 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Weather from './Weather';
+import './App.css'
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Weather Forecast App</h1>
+      <Weather />
+      
+    </div>
+  );
+}
+
+export default App;
